dc23bbdc7e55261f8491b5973639d7c0cd803359
Dmitriy Bogdanov <phenomenon13@mail.ru>
2022-06-26 13:59:57 +0300

    feat: solve task reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n > 0) {
+        n = n.toString();
+        n = n.split('');
+        n = n.reverse();
+        n = n.join('');
+        n = Number(n);
+        return n;
+    }
+    if (n < 0) {
+        n = n * (-1);
+        n = n.toString();
+        n = n.split('');
+        n = n.reverse();
+        n = n.join('');
+        n = Number(n);
+        return n;
+    }
 }
